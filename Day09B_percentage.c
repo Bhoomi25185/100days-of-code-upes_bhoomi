cbb37385c1b36c7c7cc86bef5bf0e3e3f27ef6a9
Bhoomi25185 <b.verma2523@gmail.com>
2025-11-02 21:51:38 +0530

    Add files via upload

---
+++ Day09B_percentage.c
@@ -0,0 +1,21 @@
+// Write a program that accepts a percentage (0-100) and assigns a grade based on the following criteria: 
+#include <stdio.h>
+
+int main() {
+    int percent;
+    printf("Enter percentage: ");
+    scanf("%d", &percent);
+
+    if (percent >= 90 && percent <= 100)
+        printf("Grade A\n");
+    else if (percent >= 80)
+        printf("Grade B\n");
+    else if (percent >= 70)
+        printf("Grade C\n");
+    else if (percent >= 60)
+        printf("Grade D\n");
+    else
+        printf("Grade F\n");
+
+    return 0;
+}
