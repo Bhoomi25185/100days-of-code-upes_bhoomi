9f5f6938661f28ea4173c2a4e8c81b6dfcc1fa25
Bhoomi25185 <b.verma2523@gmail.com>
2025-11-03 22:48:18 +0530

    Add files via upload

---
+++ Day37B_transpose.c
@@ -0,0 +1,23 @@
+//Find the transpose of a matrix.
+#include <stdio.h>
+
+int main() {
+    int r, c, i, j, a[100][100], t[100][100];
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+    printf("Enter elements of the matrix:\n");
+    for(i = 0; i < r; i++)
+        for(j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+    for(i = 0; i < r; i++)
+        for(j = 0; j < c; j++)
+            t[j][i] = a[i][j];
+    printf("Transpose of the matrix:\n");
+    for(i = 0; i < c; i++) {
+        for(j = 0; j < r; j++)
+            printf("%d ", t[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
+
